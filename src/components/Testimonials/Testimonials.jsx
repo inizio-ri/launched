a4237551823b9c6d9195183f3c96bca037927f9a
inizio-ri <dubeyyricha@gmail.com>
2025-12-01 05:55:51 +0530

    Final Change

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Testimonials.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { TestimonialsData } from "../../images/Testimonialsdata.js";
+import { TestimonialsData } from "../../utils/Testimonialsdata.js";
 
 
 
---
+++ src/utils/Testimonialsdata.js
@@ -0,0 +1,69 @@
+import profile1 from "../images/profile1.jpg";
+import profile2 from "../images/profile2.png";
+import profile3 from "../images/profile3.jpg";
+import profile4 from "../images/profile4.jpg";
+import profile5 from "../images/profile5.jpg";
+import profile6 from "../images/profile6.jpg";
+import profile7 from "../images/profile7.png";
+import profile8 from "../images/profile8.png";
+import profile9 from "../images/profile9.png";
+import profile10 from "../images/profile10.png";
+import profile11 from "../images/profile11.png";
+
+export const TestimonialsData = [
+    {
+        image : profile1,
+        comment: "Launched's web dev course taught me full-stack skills through real projects. I'm now working as a front-end developer.",
+        name: "Neha S"
+    },
+    {
+        image : profile2,
+        comment: "From data wrangling to predictive models, the course covered it all. I'm now a data scientist making real impact.",
+        name: "Ritika D"
+    },
+    {
+        image : profile3,
+        comment: "Launched's AI Automation program helped me streamline workflows and boost efficiency. I now lead automation projects at my company.",
+        name: "Megha T"
+    },
+    {
+        image : profile4,
+        comment: "The ML program covered model building, tuning, and deployment. I used it to land an ML engineer role.I am happy to be a part of LaunchEd",
+        name: "Siddharth M"
+    },
+    {
+        image : profile5,
+        comment: "The AI program gave me a strong foundation in machine learning and NLP. I landed an AI research role after completing it.",
+        name: "Aditya R"
+    },
+    {
+        image : profile6,
+        comment: "The data analysis course sharpened my skills in SQL, Python, and insights. I landed a data analyst role right after.",
+        name: "Arjun P"
+    },
+    {
+        image : profile7,
+        comment: "With expert guidance, I quickly secured an SDR position. The program gave me the tools to thrive in SaaS sales.",
+        name: "Kiran A"
+    },
+    {
+        image : profile8,
+        comment: "The mentor-led SaaS growth program was insightful and practical. I now help optimize growth for SaaS businesses.",
+        name: "Vikram K"
+    },
+    {
+        image : profile9,
+        comment: "Launched's SDR program gave me confidence and hands-on training. I landed my dream SaaS sales role with ease.",
+        name: "Rahul S"
+    },
+    {
+        image : profile10,
+        comment: "The program taught me data-driven SaaS growth strategies. I now help companies scale as a growth consultant.",
+        name: "Anita N"
+    },
+    {
+        image : profile11,
+        comment: "Launched's Product Management program gave me real-world product skills. I now lead product teams confidently as a PM.",
+        name: "Anusha R"
+    }
+]
